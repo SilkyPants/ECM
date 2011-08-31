59f40c07313297bc1b312eca0d0140ca43a66301
danielsilk <novacaine.nzl@gmail.com>
2011-08-31 10:53:57 +0930

    Added SQLite assembly to project Added Equation helper class and set up basic equations that will be used

---
+++ EveApi/Equations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EveApi
+{
+    /// <summary>
+    /// Static class to help work out different Eve Equations
+    /// </summary>
+    public static class Equations
+    {
+    }
+}
